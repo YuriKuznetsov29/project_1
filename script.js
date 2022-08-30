42f9e02bb720b5bb577213d7ae803cd272bb49aa
Yuri <playeer2905@gmail.com>
2022-08-30 18:25:08 +0300

    finish four lesson

--- script.js
+++ script.js
@@ -1,77 +1,73 @@
-let numberOfFilms;
-
-function start () {
-    numberOfFilms = +prompt('Какое количество фильмов вы посмотрели?', '');
-
-    while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt('Какое количество фильмов вы посмотрели?', '');
-    }
-}
-
-start ();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
-    actors : {},
+    actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: () => {
+        personalMovieDB.count = +prompt('Какое количество фильмов вы посмотрели?', '');
+
+        while (personalMovieDB.count === '' || personalMovieDB.count === null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = prompt('Какое количество фильмов вы посмотрели?', '');
+        }
+    },
+    rememberMyFilms: () => {
+        for (let i = 0; i <= 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько его оцените?', '');
+
+            if (a !== null && b !== null && a !== '' && b !== '' && a.length <= 50 && b.length <= 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done!');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 10) {
+
+            console.log('Мало');
+        } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30){
+        
+        console.log('Вы классический зритель');
+        } else if (personalMovieDB.count > 30) {
+        
+                console.log('Вы киноман');
+        }else {
+            console.log('error');
+        }
+
+    },
+    showMyDB: (hidden) => {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: () => {
+        for (let i = 1; i <= 1; i++) {
+            let generes = prompt(`Введите фильмы через ,`).toLowerCase();
+            if (generes === null || generes === '') {
+                i--;
+            }else {
+            personalMovieDB.genres = generes.split(', ');
+            personalMovieDB.genres.sort();
+            }
+        }
+        personalMovieDB.genres.forEach((value, index) => {console.log ( `Любимый жанр ${index + 1} - это ${value}` )});
+    },
+    toggleVisibleMyDB: () => {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+            console.log(personalMovieDB.privat);
+        } else {
+            personalMovieDB.privat = true;
+            console.log(personalMovieDB.privat);
+        }
+        
+    }
 
 };
 
-
-function rememberMyFilms() {
-    for (let i = 0; i <= 1; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько его оцените?', '');
-    
-        if (a !== null && b !== null && a !== '' && b !== '' && a.length <= 50 && b.length <= 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done!');
-        }else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-
-        console.log('Мало');
- } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30){
- 
- console.log('Вы классический зритель');
- } else if (personalMovieDB.count > 30) {
- 
-         console.log('Вы киноман');
- }else {
-     console.log('error');
- }
-}
-
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB (personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        const genere = prompt(`Ваш любимый жарнр под номером ${i}`);
-        personalMovieDB.genres[i - 1] = genere;
-    }
-    
-}
-
-function writeYourGenres()
-
-
-console.log(personalMovieDB);
-//test2
